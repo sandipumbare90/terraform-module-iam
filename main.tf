31a618fd5748dc0eabcd43cf7d471137c8ad70ee
sandipumbare90 <sandipumbare8@gmail>
2024-05-09 07:10:40 +0000

    IAM module for role assignment

---
+++ main.tf
@@ -0,0 +1,10 @@
+
+module "iam_binding_module" {
+  source = "terraform-google-modules/iam/google//modules/projects_iam"
+  version = "7.3"
+
+  projects = ["devops-engineer-123"]
+  bindings = {
+    "roles/compute.viwer" = ["snumbare123@gmail.com"]
+}
+}
