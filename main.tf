61eed55cd731efc90ad613273ceeafc06d8eac57
sandipumbare90 <sandipumbare8@gmail>
2024-05-10 06:24:57 +0000

    version updated

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 
 module "iam_binding_module" {
   source = "terraform-google-modules/iam/google/modules/projects_iam"
-  version = "7.3"
+  version = "~> 7.7"
+
 
   projects = ["devops-engineer-123"]
   bindings = {
