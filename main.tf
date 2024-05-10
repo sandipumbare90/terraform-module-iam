88a0699ea6fcf3d32a3acedb8980e4cdaae73140
sandipumbare90 <sandipumbare8@gmail>
2024-05-10 06:08:02 +0000

    extra / removed

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "iam_binding_module" {
-  source = "terraform-google-modules/iam/google//modules/projects_iam"
+  source = "terraform-google-modules/iam/google/modules/projects_iam"
   ###version = "7.3"
 
   projects = ["devops-engineer-123"]
