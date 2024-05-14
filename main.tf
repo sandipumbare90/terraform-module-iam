2df5f7024e8e7b87361e7a705563b912ac972daa
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2024-05-14 14:59:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "iam_binding_module" {
   source = "git::https://github.com/terraform-google-modules/terraform-google-iam"
-  version = "~> 7.7"
+  #version = "~> 7.7"
 
 
   projects = ["devops-engineer-123"]
